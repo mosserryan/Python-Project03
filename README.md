aa60dea36cf6839179def7fbda46e4e62881a5a6
mosserryan <mosser.ryan.a@gmail.com>
2022-05-27 15:12:03 -0700

    Fixed reset bug, and updated various functions. Had to scrap additional code that I couldn't finish.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Python-Project03
+
+Python GUI project where the users needs to find the treasure!
+
+For both PROJECT 02 (RPG) and PROJECT 03 (FLASK)
+
+NOTE: CSS is messy but usable, not optimized for all screen sizes. Had to remove a good chunk of the code. Made sure the project is at least operational. Some of the code became messy. I found better way to write alot of it, but instead of changing it, I went with the "save me time" route and continued down the path of messy code. Sorry Chad!
+
+WISHLIST: I misjudged how much I could do, but I was aiming to have stats, different weapons, levels, battles, sadly I needed to scrap the unfinished code :(
+
+**SPOILERS
+Find a weapon, slay the monster, grab a key to unlock the door!
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import character
 
 app = Flask(__name__)
 
+# Saving game data in a dictionary to keep track of (Ran out of time for a DB)
 game = {
     "gameover": False,
     "turn": 1,
@@ -52,31 +53,19 @@ game = {
             'monster': False
         },
         'Basement': {
-            'items': 'Treasure'
+            'items': 'Treasure',
+            'monster': False
         }
     }
 }
 
-@app.route("/gamedata", methods=["GET", "POST"])
-def jsondata():
-    if request.method == 'POST':
-        data = request.json
-        if data:
-            data = json.loads(data)
-            global game
-            game = data    
-
-    return redirect(url_for("start"))
-
-@app.route("/DungeonBunkers/<name>/<race>/<profession>/<strength>/<intelligence>/<stamina>/")
-def play(name, race, profession, strength, intelligence, stamina):
-    validMoves = game['rooms'][game['currentRoom']]
-    return render_template("game.html", name=name, race=race, profession=profession, strength=strength, intelligence=intelligence, stamina=stamina, **validMoves, inventory=game["playerInventory"], currentRoom=game["currentRoom"], turn=game["turn"], lastAction=game["lastAction"])
-
+#Starting point
+# Render the create character html
 @app.route("/")
 def start():
     return render_template("create_character.html")
-
+# Once user submits the created characters data it will use the data and save it into the game dict
+# It also passed the characters value to the play url to kick of the game.
 @app.route("/create", methods=["POST"])
 def createCharacter():
     if request.method == "POST":
@@ -88,6 +77,27 @@ def createCharacter():
             game['player'] = player
             return redirect(url_for("play", name=player.name, race=player.race, profession=player.profession, strength=player.stats['strength'], intelligence=player.stats['intelligence'], stamina=player.stats['stamina']))
 
+
+# When hitting this it allows user to alter game, main purpose is for game reset.
+@app.route("/gamedata", methods=["GET", "POST"])
+def jsondata():
+    if request.method == 'POST':
+        data = request.json
+        if data:
+            data = json.loads(data)
+            global game
+            game = data    
+    return jsonify(data)
+
+# Pass data to the route to for game to update the html with the values
+@app.route("/DungeonBunkers/<name>/<race>/<profession>/<strength>/<intelligence>/<stamina>/")
+def play(name, race, profession, strength, intelligence, stamina):
+    validMoves = game['rooms'][game['currentRoom']]
+    # I remembered how to send more than just a variable at a time too late in the process. (**)
+    # Return is too long and messy, time premitting I would have clean this up.
+    return render_template("game.html", name=name, race=race, profession=profession, strength=strength, intelligence=intelligence, stamina=stamina, **validMoves, inventory=game["playerInventory"], currentRoom=game["currentRoom"], turn=game["turn"], lastAction=game["lastAction"], gameover=game['gameover'])
+
+# All the code the grabs the users actions (movements, attack, pickup, and reset)
 @app.route("/action", methods=["POST"])
 def action():
     currentRoom = game['currentRoom']
@@ -117,7 +127,7 @@ def action():
                 if(game["currentRoom"] == 'Hall' and 'key' in game['playerInventory']):
                     game["currentRoom"] = game['rooms'][currentRoom]["west"]
                     game['turn'] += 1
-                    game['lastAction'] = f"You go {direction} into the {game['currentRoom']}, you find a treasure chest. Use the key!"
+                    game['lastAction'] = f"You go {direction} into the {game['currentRoom']}, you find a treasure chest. Pick it up!"
                 elif(game["currentRoom"] == 'Hall' and 'key' not in game['playerInventory']):
                     game['lastAction'] = f"You try to {direction} but the door is locked. Looks like you need to find a key!"
                 else:
@@ -125,10 +135,14 @@ def action():
                     game['turn'] += 1
                     game['lastAction'] = f"You go {direction} into the {game['currentRoom']}"
         elif request.form.get("getItem"):
-            game['playerInventory'].append(game['rooms'][currentRoom]['items'])
-            game['lastAction'] = f"You pick up a {game['rooms'][currentRoom]['items']}"
-            game['turn'] += 1
-            del game['rooms'][currentRoom]['items']
+            if(game['rooms'][currentRoom]['items'] == 'Treasure'):
+                 game['lastAction'] = f"You pick up all the {game['rooms'][currentRoom]['items']} you win! Click reset to play again!"
+                 del game['rooms'][currentRoom]['items']
+            else:    
+                game['playerInventory'].append(game['rooms'][currentRoom]['items'])
+                game['lastAction'] = f"You pick up a {game['rooms'][currentRoom]['items']}"
+                game['turn'] += 1
+                del game['rooms'][currentRoom]['items']
         elif request.form.get("attack"):
             if(game["rooms"][currentRoom]['monster'] == True):
                 if("sword" in game['playerInventory']):
@@ -137,13 +151,13 @@ def action():
                     game["rooms"][currentRoom]['monster'] = False
                     game['turn'] += 1
                 else:
-                    game['lastAction'] = f"Swing your fists at the Monster! That was embarassing, the monster one shots you, you are D E D ded. GAME OVER."
                     game['gameover'] = True
             else:
                 game['lastAction'] = f"There is nothing to attack in this room."
                 game['turn'] += 1
         elif request.form.get("reset"):
             resetGame()
+            return redirect(url_for("start"))
         return redirect(url_for("play", name=game["player"].name, race=game["player"].race, profession=game["player"].profession, strength=game["player"].stats['strength'], intelligence=game["player"].stats['intelligence'], stamina=game["player"].stats['stamina']))
 
 def resetGame():
@@ -193,7 +207,7 @@ def resetGame():
     }
 }
     game = json.dumps(game)
-    requests.post(URL, json=game)
+    return requests.post(URL, json=game)
 
 
 if __name__ == "__main__":
